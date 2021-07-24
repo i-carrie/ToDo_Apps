1432208090d59fb4d2d7227b7b9ffb78b3cca818
i-carrie <50307972+i-carrie@users.noreply.github.com>
2021-07-24 09:59:14 +0900

    Created App.jsx for preparation

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,42 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="Create New ToDo" />
+        <button>Add</button>
+      </div>
+      <div>
+        <p>Work In Progress</p>
+        <ul>
+          <div>
+            <li>abcd</li>
+            <button>Done</button>
+            <button>Delete</button>
+          </div>
+          <div>
+            <li>xyz</li>
+            <button>Done</button>
+            <button>Delete</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>Done - Closed Items</p>
+        <ul>
+          <div>
+            <li>abcd</li>
+            <button>Done</button>
+            <button>Delete</button>
+          </div>
+          <div>
+            <li>xyz</li>
+            <button>Done</button>
+            <button>Delete</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
