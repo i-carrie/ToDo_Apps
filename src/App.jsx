282a8084a403fc3b5b3d0e2c8e9ff62425157642
i-carrie <50307972+i-carrie@users.noreply.github.com>
2021-07-24 11:32:18 +0900

    Modified App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState(["abcd", "xyz"]);
+  const [completeTodos, setCompleteTodos] = useState(["zzz"]);
+  const [todoText, setTodoText] = useState("");
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="Create New ToDo" />
+        <input
+          placeholder="Create New ToDo"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
         <button>Add</button>
       </div>
       <div className="incomplete-area">
         <p className="title">Work In Progress</p>
         <ul>
-          <div className="list-row">
-            <li>abcd</li>
-            <button>Done</button>
-            <button>Delete</button>
-          </div>
-          <div className="list-row">
-            <li>xyz</li>
-            <button>Done</button>
-            <button>Delete</button>
-          </div>
+          {incompleteTodos.map((todo) => {
+            return (
+              <>
+                <div key={todo} className="list-row">
+                  <li>{todo}</li>
+                  <button>Done</button>
+                  <button>Delete</button>
+                </div>
+              </>
+            );
+          })}
         </ul>
       </div>
       <div className="completed-area">
         <p className="title">Done - Closed Items</p>
         <ul>
-          <div className="list-row">
-            <li>abcd</li>
-            <button>Retrieve</button>
-          </div>
+          {completeTodos.map((todo) => {
+            return (
+              <>
+                <div key={todo} className="list-row">
+                  <li>{todo}</li>
+                  <button>Retrieve</button>
+                </div>
+              </>
+            );
+          })}
         </ul>
       </div>
     </>
